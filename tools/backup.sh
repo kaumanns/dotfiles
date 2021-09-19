3b19db49c0cd1e2e248f5e73cb6579cba59af9ec
david <david@carbi>
2021-09-19 13:04:17 +0200

    Added snapshot submodule and replaced tool script for backup with 'make backup' command

--- tools/backup.sh
+++
@@ -1,5 +0,0 @@
-# Remote
-# ~/git/snapshot/snapshot.sh -s ~ -d pi@192.168.178.39:/media/2TB/backups -p -i ~/.rsync/include
-
-# Local
-~/git/snapshot/snapshot.sh -s ~ -d /run/media/david/2TB/backups -p -i ~/.rsync/include
